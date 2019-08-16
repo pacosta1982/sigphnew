18a067a8cbb5a546e815b8a993e1488953bbea83
pacosta1982 <acosta.aleoli@gmail.com>
2019-08-16 10:31:53 -0400

    ajustes postulantes fecha

--- app/Models/Postulante.php
+++ app/Models/Postulante.php
@@ -7,10 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Postulante extends Model
 {
     //
-    public function getDateFormat()
-    {
-        return 'Y-d-m H:i:s.v';
-    }
+    protected $dateFormat = 'Y-m-d H:i:s.v';
+
+    protected $dates = ['created_at','updated_at'];
 
     protected $fillable = ['first_name', 'last_name', 'cedula','marital_status','nacionalidad','gender','birthdate','localidad',
     'asentamiento','ingreso','address','grupo','phone','mobile'];
