928816aa126f700718bc241e5571dd3c4124f206
pacosta1982 <acosta.aleoli@gmail.com>
2019-08-16 10:50:40 -0400

    ajustes

--- app/Models/Postulante.php
+++ app/Models/Postulante.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Postulante extends Model
 {
     //
-    protected $dateFormat = 'Y-m-d H:i:s.v';
+    //protected $dateFormat = 'Y-m-d H:i:s.v';
 
-    protected $dates = ['created_at','updated_at'];
+    protected function getDateFormat()
+    {
+        return 'Y-m-d H:i:s.u0';
+    }
+
+    //protected $dates = ['created_at','updated_at'];
 
     protected $fillable = ['first_name', 'last_name', 'cedula','marital_status','nacionalidad','gender','birthdate','localidad',
     'asentamiento','ingreso','address','grupo','phone','mobile'];
