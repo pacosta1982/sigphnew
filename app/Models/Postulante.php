4597ae4a075499b4ec5645b10d990089b2e8f08b
pacosta1982 <acosta.aleoli@gmail.com>
2019-08-16 11:16:31 -0400

    as

--- app/Models/Postulante.php
+++ app/Models/Postulante.php
@@ -7,23 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Postulante extends Model
 {
     //
-    //protected $dateFormat = 'Y-m-d H:i:s.v';
+    protected $dateFormat = 'Y-m-d H:i:s.v';
 
-    public function getDateFormat()
-    {
-        return 'Y-m-d H:i:s.v';
-    }
 
-    /**
-     * Convert a DateTime to a storable string.
-     *
-     * @param  \DateTime|int  $value
-     * @return string
-     */
-    public function fromDateTime($value)
-    {
-        return $value->format('Y-m-d H:i:s.v');
-    }
 
     //protected $dates = ['created_at','updated_at'];
 
