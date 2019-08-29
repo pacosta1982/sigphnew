d01a5fcc8b2c042243668f0b7db0e79495565f42
pacosta1982 <acosta.aleoli@gmail.com>
2019-08-29 10:25:44 -0400

    ajustes pdf

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -7,15 +7,16 @@
     <img src="{{public_path('img/logofull.png')}}" class="imagencentro" width="650" >
     <h2>Proyecto: {{$project->name}}</h2>
     <h4>Código: {{$project->id}}</h4>
-    <h4>Programa: FONAVIS</h4>
           <p>
-          <strong>SAT:</strong>{{$project->sat_id?$project->getSat->NucNomSat:""}}<br>
+            <strong>Programa: FONAVIS</strong>
+            <strong>SAT:</strong>{{$project->sat_id?$project->getSat->NucNomSat:""}}<br>
             <strong>Departamento: </strong>{{$project->state_id?$project->getState->DptoNom:""}}<br>
             <strong>Distrito:</strong> {{$project->city_id}}<br>
             <strong>Modalidad:</strong> {{$project->modalidad_id?$project->getModality->name:""}}<br>
             <strong>Tipo de Terreno:</strong> {{$project->land_id?$project->getLand->name:""}}<br>
             <strong>Total Postulantes:</strong> {{ $postulantes->count() }}<br>
           </p>
+
       <h2>Lista de Postulantes</h2>
     <table class="table">
         <tbody>
