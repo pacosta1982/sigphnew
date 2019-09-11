6eea82377aa9254314e6102f76abb82932a94192
pacosta1982 <acosta.aleoli@gmail.com>
2019-09-11 10:54:49 -0400

    ajustes eliminar

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -71,10 +71,12 @@
                 <a href="{{asset('images/')}}/{{$project->id."/project/general/".$doc->file_path}}" target="_blank">
                     <button class="btn btn-success" type="button"><i class="fa fa-download"></i> Descargar </button>
                 </a>
+                @if (!isset($project->getEstado->stage_id))
                 <button class="btn btn-danger feed-id" type="button" data-toggle="modal"
                 data-target="#modal-danger" data-id="{{ $doc->id }}" data-title="{{ $doc->title }}">
                     <i class="fa fa-close"></i> Eliminar
                 </button>
+                @endif
             </td>
           </tr>
             @endforeach
